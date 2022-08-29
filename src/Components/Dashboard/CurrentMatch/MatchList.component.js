e80946db76267b48a9f60a43bd8967ce9298300d
yorktreinies <york.privat@gmail.com>
2022-08-29 11:02:32 +0200

    CurrentMatch; MatchList-Viewer

---
+++ src/Components/Dashboard/CurrentMatch/MatchList.component.js
@@ -0,0 +1,11 @@
+import Box from "@mui/material/Box";
+
+const MatchList = () => {
+  return (
+    <Box>
+      List
+    </Box>
+  );
+};
+
+export default MatchList;
---
+++ src/Components/Dashboard/CurrentMatch/MatchViewer.component.js
@@ -0,0 +1,11 @@
+import Box from "@mui/material/Box";
+
+const MatchViewer = () => {
+  return (
+    <Box>
+      Details
+    </Box>
+  );
+};
+
+export default MatchViewer;
