b95845ec0c4a5868ba01297ddfc91e90cbce8901
yorktreinies <york.privat@gmail.com>
2022-08-29 11:00:50 +0200

    themes

--- src/App.js
+++ src/App.js
@@ -11,27 +11,38 @@ import Standings from "./Components/Dashboard/Standings.bundle";
 import OverlayPage from "./Pages/Overlay.page";
 import Overview from "./Components/Dashboard/Overview.bundle";
 import Login from "./Components/Website/Login.bundle";
+import { createTheme, ThemeProvider } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkMode = createTheme({
+  palette: {
+    mode: "light",
+  }
+});
 
 const App = () => {
   return (
     <StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="dashboard" element={<DashboardPage />}>
-            <Route path="" element={<Overview />} />
-            <Route path="current" element={<CurrentMatch />} />
-            <Route path="standings" element={<Standings />} />
-            <Route path="database" element={<DatabaseEditor />} />
-          </Route>
-          <Route path="/" element={<WebsitePage />}>
-            <Route path="" element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="signout" element={<About />} />
-          </Route>
-          <Route path="overlay" element={<OverlayPage />} />
-          <Route path="login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={darkMode}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Routes>
+            <Route path="dashboard" element={<DashboardPage />}>
+              <Route path="" element={<Overview />} />
+              <Route path="current" element={<CurrentMatch />} />
+              <Route path="standings" element={<Standings />} />
+              <Route path="database" element={<DatabaseEditor />} />
+            </Route>
+            <Route path="/" element={<WebsitePage />}>
+              <Route path="" element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="signout" element={<About />} />
+            </Route>
+            <Route path="overlay" element={<OverlayPage />} />
+            <Route path="login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </StrictMode>
   );
 };
