487fb88cde66e1f8d89945d0cac7eb7ec6496898
yorktreinies <york.privat@gmail.com>
2022-10-13 17:08:19 +0200

    LICENSE.md added

--- src/Components/Dashboard/CurrentMatch/GeneralSettings.component.js
+++ src/Components/Dashboard/CurrentMatch/GeneralSettings.component.js
@@ -1,16 +1,65 @@
 import { TextField } from "@mui/material";
 import Typography from "@mui/material/Typography";
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { forwardRef, memo, useImperativeHandle, useState } from "react";
 
-const GeneralSettings = () => {
+const GeneralSettings = forwardRef((props, _ref) => {
+  const [match, setMatch] = useState({
+    name: "",
+    format: "",
+    date: new Date()
+  });
+
+  const handleDateChange = (newDate) => {
+    setMatch({...match, date: newDate._d});
+  }
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    let newState = {...match, [e.target.name]: e.target.value}
+    setMatch(newState);
+  }
+
+  useImperativeHandle(_ref, () => ({
+    getSettings: () => {
+      return match;
+    },
+  }));
 
   return (
-    <>
+    <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="de">
       <Typography>General</Typography>
-      <TextField fullWidth size="small" variant="outlined" label="Name" />
-      <TextField sx={{mt: 1}} fullWidth size="small" variant="outlined" label="Date" />
-      <TextField sx={{mt: 1}} fullWidth size="small" variant="outlined" label="Format" />
-    </>
+      <TextField
+        fullWidth
+        size="small"
+        variant="outlined"
+        label="Name"
+        name="name"
+        value={match.name}
+        onChange={handleChange} />
+      <DateTimePicker
+        inputFormat="DD/MM/YYYY HH:mm"
+        ampm={false}
+        disablePast={true}
+        onChange={handleDateChange}
+        label="Date"
+        value={match.date}
+        renderInput={(params) => <TextField fullWidth sx={{mt: 1}} size="small" {...params} />}
+      />
+      <TextField
+        sx={{mt: 1}}
+        fullWidth
+        size="small"
+        variant="outlined"
+        label="Format"
+        name="format"
+        value={match.format}
+        onChange={handleChange}/>
+    </LocalizationProvider>
   )
-};
+});
 
-export default GeneralSettings;
+GeneralSettings.displayName = "GeneralSettingsComponent";
+
+export default memo(GeneralSettings);
--- src/Components/Dashboard/CurrentMatch/MatchViewer.component.js
+++ src/Components/Dashboard/CurrentMatch/MatchViewer.component.js
@@ -5,14 +5,22 @@ import TeamSettings from "./TeamSettings.component";
 import GeneralSettings from "./GeneralSettings.component";
 import { Add, Save, Update } from "@mui/icons-material";
 import Divider from "@mui/material/Divider";
+import { useRef } from "react";
 
 const MatchViewer = () => {
+  const generalSettingsRef = useRef();
+
+  const handleUpdate = () => {
+    const general = generalSettingsRef.current?.getSettings();
+    console.log(general);
+  }
+
   return (
     <Box sx={{ pl: 1, mr: 1, height: 'calc(100vh - 60px)', borderLeft: '1px solid rgba(0,0,0,0.2)'}}>
       <Typography variant="h5">Edit Match</Typography>
       <Grid container spacing={1}>
         <Grid item xs={12}>
-          <GeneralSettings />
+          <GeneralSettings ref={generalSettingsRef}/>
           <Divider sx={{mt:1}}/>
           <Typography sx={{mt:1}}>Teams</Typography>
         </Grid>
@@ -30,7 +38,13 @@ const MatchViewer = () => {
         <Save sx={{mr: 1}} />
         Save
       </Fab>
-      <Fab sx={{position: 'fixed', bottom: 15, right: 15}} size="medium" variant="extended" color="primary" aria-label="update">
+      <Fab
+        sx={{position: 'fixed', bottom: 15, right: 15}}
+        size="medium"
+        variant="extended"
+        color="primary"
+        aria-label="update"
+        onClick={handleUpdate}>
         <Update sx={{mr: 1}} />
         Update
       </Fab>
