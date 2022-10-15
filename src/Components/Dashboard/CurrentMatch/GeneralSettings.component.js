1302cc62d7b97f40845e4beb8a0d21e2898efb5f
yorktreinies <york.privat@gmail.com>
2022-10-15 03:02:53 +0200

    Nom nom new changes

--- src/Components/Dashboard/CurrentMatch/GeneralSettings.component.js
+++ src/Components/Dashboard/CurrentMatch/GeneralSettings.component.js
@@ -1,4 +1,4 @@
-import { TextField } from "@mui/material";
+import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
@@ -7,7 +7,7 @@ import { forwardRef, memo, useImperativeHandle, useState } from "react";
 const GeneralSettings = forwardRef((props, _ref) => {
   const [match, setMatch] = useState({
     name: "",
-    format: "",
+    format: "Bo3",
     date: new Date()
   });
 
@@ -17,8 +17,10 @@ const GeneralSettings = forwardRef((props, _ref) => {
 
   const handleChange = (e) => {
     e.preventDefault();
-    let newState = {...match, [e.target.name]: e.target.value}
+    let newState = {...match, [e.target.name]: e.target.value};
+    console.log(newState);
     setMatch(newState);
+    console.log(match);
   }
 
   useImperativeHandle(_ref, () => ({
@@ -44,18 +46,29 @@ const GeneralSettings = forwardRef((props, _ref) => {
         disablePast={true}
         onChange={handleDateChange}
         label="Date"
+        name="date"
         value={match.date}
         renderInput={(params) => <TextField fullWidth sx={{mt: 2}} size="small" {...params} />}
       />
-      <TextField
-        sx={{mt: 2}}
-        fullWidth
-        size="small"
-        variant="outlined"
-        label="Format"
-        name="format"
-        value={match.format}
-        onChange={handleChange}/>
+      <FormControl fullWidth>
+        <InputLabel sx={{mt: 2}} size="small" id="format-select-label">Format</InputLabel>
+        <Select
+          sx={{mt: 2}}
+          size="small"
+          labelId="format-select-label"
+          id="format-select"
+          label="Format"
+          name="format"
+          value={match.format}
+          onChange={handleChange}
+        >
+          <MenuItem value="Bo1">Best of 1</MenuItem>
+          <MenuItem value="Bo3">Best of 3</MenuItem>
+          <MenuItem value="Bo5">Best of 5</MenuItem>
+          <MenuItem value="Bo7">Best of 7</MenuItem>
+          <MenuItem value="Bo9">Best of 9</MenuItem>
+        </Select>
+      </FormControl>
     </LocalizationProvider>
   )
 });
--- src/Components/Dashboard/CurrentMatch/TeamSettings.component.js
+++ src/Components/Dashboard/CurrentMatch/TeamSettings.component.js
@@ -1,10 +1,31 @@
-import { TextField } from "@mui/material";
-
 const TeamSettings = ({side}) => {
 
   return (
-    <TextField fullWidth size="small" variant="outlined" label={"Team " + side} />
-  )
+    <>
+      <TextField fullWidth size="small" variant="outlined" label={"Team " + side} />
+      <FormControl fullWidth>
+        <InputLabel sx={{mt: 2}} size="small" id={side + "-wins-select-label"}>Wins</InputLabel>
+        <Select
+          sx={{mt: 2}}
+          size="small"
+          labelId={side + "-wins-select-label"}
+          id={side + "-wins-select"}
+          label="Wins"
+        >
+          <MenuItem value={0}>0</MenuItem>
+          <MenuItem value={1}>1</MenuItem>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={3}>3</MenuItem>
+          <MenuItem value={4}>4</MenuItem>
+          <MenuItem value={5}>5</MenuItem>
+          <MenuItem value={6}>6</MenuItem>
+          <MenuItem value={7}>7</MenuItem>
+        </Select>
+      </FormControl>
+    </>
+)
 };
+
+import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 
 export default TeamSettings;
